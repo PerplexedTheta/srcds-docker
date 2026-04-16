3046767447a092c9bad652bd52f76df6cbadd59f
Jake Deery <jake.deery@openfifth.co.uk>
2026-04-16 21:34:48 +0100

    fix: chown shouldnt crash srcds

--- files/src/entrypoint.sh
+++ files/src/entrypoint.sh
@@ -15,10 +15,10 @@ fi
 usermod -u ${USER_UID:-1000} steam || exit 1
 groupmod -g ${USER_GID:-1000} steam || exit 1
 
-chown -R steam /home/steam || exit 1
-chgrp -R steam /home/steam || exit 1
+chown -R steam /home/steam
+chgrp -R steam /home/steam
 
-chown -R steam /srcds || exit 1
-chgrp -R steam /srcds || exit 1
+chown -R steam /srcds
+chgrp -R steam /srcds
 
 exec su steam /bin/bash -c '/run_srcds.sh'
